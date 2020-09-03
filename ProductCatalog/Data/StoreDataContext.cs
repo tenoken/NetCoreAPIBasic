6f95c9ecb37bae22a5def93eeb0535dea6c23dad
tenoken <shiroiparadina@gmail.com>
2020-09-02 22:52:10 -0300

    First Commit

---
+++ ProductCatalog/Data/StoreDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data.Maps;
+using ProductCatalog.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost,1400;Database=prodcat;User ID=SA;Password=@oisora17");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductMap());
+            builder.ApplyConfiguration(new CategoryMap());
+        }
+    }
+}
---
+++ ProductCatalog/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
